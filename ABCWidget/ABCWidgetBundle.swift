ada01abb41565f05663be50b512f253c9bbcfe68
Darul Firmansyah <darulfirmansyah@gmail.com>
2024-08-06 06:23:18 +0700

    Setup Widget

---
+++ ABCWidget/ABCWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ABCWidgetBundle.swift
+//  ABCWidget
+//
+//  Created by Destriana Orchidea on 06/08/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ABCWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ABCWidget()
+    }
+}
